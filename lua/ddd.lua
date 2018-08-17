75aec56928a5f3f1b507a731956a3eac809ae55d
zirpon-hero-win10 <735037139@qq.com>
2018-08-17 15:54:23 +0800

    weekly report

---
+++ lua/ddd.lua
@@ -0,0 +1,37 @@
+local usertable = {}
+
+local meta = {}
+meta.n = 0
+meta.__newindex = function (t, k, v)
+--    print("__newindex", k, v)
+    if k == "n" then
+        return nil
+    end
+    if not v then
+        meta.n = (meta.n < 1 and 0) or (meta.n-1)
+    else
+        meta.n = meta.n + 1
+    end
+
+    t[k] = v
+end
+meta.__index = function(t,k)    
+--    print("__index", k)
+    if k == "n" then
+        return meta.n
+    else
+        return t[k]
+    end
+end
+
+setmetatable(usertable, meta)
+usertable.name = "kawaii"
+print(usertable.n) 
+print(#usertable)
+for i,v in pairs(usertable) do
+    print(i,v)
+end
+usertable.name = nil
+print(usertable.n) 
+usertable.n = 90
+print(usertable.n)
