d6de3ff1abb07cf9cc7203213b4c5ba7427f690a
zirpon <735037139@qq.com>
2018-10-13 18:25:16 +0800

    week report

--- lua/test2.lua
+++ lua/test2.lua
@@ -3,3 +3,11 @@ oo.k = 10
 local t = oo.k
 oo.k = 9
 print(t)
+
+function function_name( a, b )
+    a % b == a - math.floor(a/b)*b  
+end
+
+function domod( a,b )
+    return a - math.floor(a/b)*b
+end
